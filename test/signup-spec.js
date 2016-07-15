8ccd0b0b19d6484ca825f9a39617aabfe11a7ef6
Rachel Paresa <rparesa@uw.edu>
2016-07-15 00:25:14 -0700

    Add email protractor code

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 describe("Submit form button", function() {
 
@@ -17,5 +17,41 @@ describe("Submit form button", function() {
 		expect(submitButton.isEnabled()).toEqual(false);
 
 	});
+
+});
+
+
+describe('Signup Metadata', function(){
+	it('should have the correct title', function(){
+
+	});
+});
+
+describe('Initial Page Appearnace', function(){
+	it('should have', function(){
+
+	});
+});
+
+describe('Sign up Form', function() {
+	
+	it('should be valid if email is valid', function(){
+		var emailInput = element(by.css("#email"));
+		var button = element(by.css("#submit"));
+		//should be invalid if empty
+		emailInput.sendKeys('');
+		expect(button.isEnabled()).toEqual(false);
+
+		//should be invalid if not proper format
+		emailInput.clear(); 
+		emailInput.sendKeys('aaa');
+		expect(button.isEnabled()).toEqual(false);
+
+		//should be valid if email is valid
+		emailInput.clear(); 
+		emailInput.sendKeys('cat@gmail.com');
+		expect(button.isEnabled()).toEqual(true);
+	});
+
 
 });
